92e39d44aab280590edd381be1e34910cb224b90
Wlad loban <qwegadqwe@gmail.com>
2023-09-13 22:15:07 +0300

    feat: notification toast, provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,16 @@
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from '@/store/store.ts';
 import App from '@/app/App.tsx';
+import Toast from '@/components/ui-toolkit/toastContainer/Toast.tsx';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import './styles/index.scss';
 
-createRoot(document.getElementById('root')!).render(<App />);
+createRoot(document.getElementById('root')!).render(
+  <Provider store={store}>
+    <App />
+    <Toast />
+  </Provider>,
+);
