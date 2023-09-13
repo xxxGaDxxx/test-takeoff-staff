fe2a51ea68b12c0be8eef06f82badd22aa0e70eb
Wlad loban <qwegadqwe@gmail.com>
2023-09-13 23:28:32 +0300

    feat: contactsApi

---
+++ src/api/contactsApi.ts
@@ -0,0 +1,40 @@
+import { instance } from '@/api/config.ts';
+import {
+  AddedContactArgs,
+  DeleteContactArgs,
+  GetContactsArgs,
+  UpdateContactArgs,
+} from '@/api/type.ts';
+
+const contactsApi = {
+  getContacts({ userId, accessToken }: GetContactsArgs) {
+    return instance.get(`/contacts?userId=${userId}`, {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    });
+  },
+  addContacts({ accessToken, ...restArgs }: AddedContactArgs) {
+    return instance.post(
+      '/contacts',
+      { ...restArgs },
+      {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      },
+    );
+  },
+  deleteContacts(params: DeleteContactArgs) {
+    return instance.delete<ResponseType>(`contacts/${params.contactId}`, {
+      headers: { Authorization: `Bearer ${params.accessToken}` },
+    });
+  },
+  updateContacts({ contactId, accessToken, ...resArgs }: UpdateContactArgs) {
+    return instance.patch(
+      `contacts/${contactId}`,
+      { ...resArgs },
+      {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      },
+    );
+  },
+};
+
+export default contactsApi;
