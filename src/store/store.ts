afea5aa8a5b5ba7f2f8bce9627dd088d37840edb
Wlad loban <qwegadqwe@gmail.com>
2023-09-14 22:11:21 +0300

    chore: Remove window.store (debugging code)

--- src/store/store.ts
+++ src/store/store.ts
@@ -20,7 +20,3 @@ export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export default store;
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-window.store = store;
