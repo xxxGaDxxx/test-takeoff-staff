64de1d109bd17ed39b6af129893e5fc4824f933b
Wlad loban <qwegadqwe@gmail.com>
2023-09-14 16:23:19 +0300

    feat: loader component

---
+++ src/components/ui-toolkit/loader/Loader.tsx
@@ -0,0 +1,9 @@
+import styles from './Loader.module.scss';
+
+const Loader = () => (
+  <div className={styles.loaderContainer}>
+    <div className={styles.loader} />
+  </div>
+);
+
+export default Loader;
